7d37ea94d6d55b206321450499f130c703b1ca31
Matt Robertson <sile16@gmail.com>
2015-08-12 10:00:41 -0500

    major update of html generation

--- makeweb.py
+++ makeweb.py
@@ -9,6 +9,16 @@ import time
 import sys
 from subprocess import call
 
+def headers(f,headers):
+    f.write('<tr>')
+    for header in headers:
+        f.write('<th>%s</th>' % str(header) )
+    f.write('</tr>')
+
+def cell(f,color,msg):
+    if color is None:
+        color = 'AliceBlue'
+    f.write('<td bgcolor="%s">%s</td>' % (color,str(msg)) )
 
 def make_html(state,html_file):
     #make build_status.html 
@@ -18,10 +28,49 @@ def make_html(state,html_file):
 
     with open(html_file,'w') as f:
         f.write('<table>')
-        for c in state['commits']:
+        headers(f,['commit','author dt','commit dt','Docker Date','test:app', 'test:chain', 'test:intense', 'test:perf','avatar','author','msg','logs'])
+        for sha in state['commits']:
+            build = state['commits'][sha]
+            c = build['commit']
             f.write('<tr>')
-            f.write('<td>%s</td><td>%s</td><td>%s</td>' % (c, str(state['commits'][c]['commit'].commit.author.date),str(state['commits'][c]['rc'])))
+            
+
+            push_date = None
+            if build['rc']['cli'] == 0 and build['rc']['witness'] == 0:
+               if 'docker_push_date' in build:
+                   build_color = 'LightGreen'
+                   push_date = build['docker_push_date']
+               else:
+                   build_color = 'AliceBlue'
+            else:
+                build_color = 'OrangRed'
+            
+   
+            cell(f,build_color,'<a href="%s">%s</a>' % (c.html_url, sha[0:7]) )
+            cell(f,build_color,c.commit.author.date )
+            cell(f,build_color,c.commit.committer.date )
+            cell(f,build_color,push_date)            
+
+            if 'tests' in build:
+                for t in sorted(build['tests']):
+                    if build['tests'][t]['rc'] == 0:
+                        t_color = 'LightGreen'
+                    else:
+                        t_color = 'OrangeRed'
+                    cell(f,t_color, "{:.1f}".format(build['tests'][t]['duration']) ) 
+            else:
+                cell(f,'OrangeRed',"--")
+                cell(f,'OrangeRed',"--")
+                cell(f,'OrangeRed',"--")
+                cell(f,'OrangeRed',"--")
+
+            cell(f,None,'<img src="%s"/ height="42" width="42">' % c.author.avatar_url )
+            cell(f,build_color,c.commit.author.name)
+            cell(f,build_color,c.commit.message )
+            cell(f,build_color,'<a href="%s">logs</a>' % sha) 
+                
             f.write('</tr>')
+
         f.write('</table>')
         
 
